085e78264828f5fea0bf56f52a2148c744b6f1be
hyeonjin <gdalove@naver.com>
2022-07-04 13:27:30 +0900

    프로필 2차 업데이트

--- js/main.js
+++ js/main.js
@@ -15,13 +15,29 @@ window.addEventListener('load', (e) => {
   visual.classList.add('on');
 })
 
+//scroll
+const section = document.querySelectorAll('.section');
+const totalNum = section.length;
+window.addEventListener("scroll", function (e) {
+  const scroll = this.scrollY;
+  for (let i = 0; i < totalNum; i++) {
+    if (scroll > section[i].offsetTop - window.outerHeight / 2.5 && scroll < section[i].offsetTop - window.outerHeight / 2.5 + section[i].offsetHeight) {
+      section[i].classList.add('on');
+    } else {
+      section[i].classList.remove('on');
+    }
+  }
+
+})
 
 
 
+//mask motion
   let scrollBody = document.querySelector('.visual');
   let maskLeft = scrollBody.querySelector('.left_box');
   let maskRight = scrollBody.querySelector('.right_box');
-
+  let titText = scrollBody.querySelector('.start_txt');
+  let endingContent = scrollBody.querySelector('.ending_txt');
 
   let scrollHeight;
   let sectionOffsetTop;
@@ -30,13 +46,11 @@ window.addEventListener('load', (e) => {
   let winScrollTop;
   let scrollPerecnt;
   let percent;
-
   function changeOverlap() {
 
     setProperty();
     motionRender();
   };
-
   function setProperty() {
 
     scrollHeight = scrollBody.offsetHeight;
@@ -55,7 +69,29 @@ window.addEventListener('load', (e) => {
     let maskVal = Math.max(maskEndValue, maskStartValue - (scrollPerecnt * maskStartValue));
     maskLeft.style.width = maskVal + '%'
     maskRight.style.width = maskVal + '%';
-  };
+    
+    if (percent > 20) {
+      titText.classList.add('active');
+    } else {
+      titText.classList.remove('active');
+    }
+
+    if (percent >= 70) {
+      endingContent.classList.add('active');
+    }
+    else {
+      endingContent.classList.remove('active');
+    }
+
+    if(percent > 100){
+      endingContent.classList.remove('active');
+    }
+   
+
+    console.log(percent)
+
+  }
+
 
   function init() {
     changeOverlap();
